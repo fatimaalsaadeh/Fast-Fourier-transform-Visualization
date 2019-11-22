257c0733ff47e9032536598c96d056c353d812ad
ashdunn <31973458+ashdunn@users.noreply.github.com>
2019-11-22 00:57:45 -0500

    step through some of divide and conquer

--- fft.py
+++ fft.py
@@ -8,24 +8,24 @@ def polar_plot(cn):  # polar plot of complex number using the angle and
     ax2.plot([0, np.angle(cn)], [0, abs(cn)], marker='o')
 
 
-def fast_fourier_transform(samples, root):  # FFT algorithm starts here
+def fast_fourier_transform(samples, root, count):  # FFT algorithm starts here
     n = len(samples)  # length of the samples
 
     if n == 1:  # base case
         return samples
 
+    # setup to show only one left/right pair at each level of recursion
+    passonL = 0
+    passonR = 0
+    if count == 2:
+        passonL = 2
+        passonR = 1
+
     left_side = [num for index, num in enumerate(samples) if index % 2 == 0]  # left side indices
     right_side = [num for index, num in enumerate(samples) if index % 2 == 1]  # right side indices
 
-    #plt.scatter(left_side, np.zeros_like(left_side), alpha=.9)
-    #plt.scatter(right_side, np.zeros_like(right_side), alpha=.4)
-
-    #plt.yticks([])
-    #plt.show()
-
-
-    transformed_left = fast_fourier_transform(left_side, root * root)  # recursive on the left side
-    transformed_right = fast_fourier_transform(right_side, root * root)  # recursive on the right side
+    transformed_left = fast_fourier_transform(left_side, root * root, passonL)  # recursive on the left side
+    transformed_right = fast_fourier_transform(right_side, root * root, passonR)  # recursive on the right side
 
     m_root = 1  # root complex number multiplier for the power of it
 
@@ -35,10 +35,42 @@ def fast_fourier_transform(samples, root):  # FFT algorithm starts here
         r[int(j + n / 2)] = transformed_left[j] - m_root * transformed_right[j]
         m_root = m_root * root
 
+    # plot if designated
+    print(count)
+    if count:
+        fig = plt.figure(figsize=(5, 10))  # Initialize the figure
+
+        ax2 = fig.add_subplot(412, projection='polar')  # Initialize the second polar plot
+
+        ax1 = fig.add_subplot(411)  # Initialize the first sine wave plot
+        plt.ylabel("Amplitude")
+        plt.xlabel("Time [s]")
+        ax1.plot(t, s)
+
+        ti = t[1] - t[0]  # Take a discrete interval from the signal time domain
+        f = np.linspace(0, 1 / ti, sn)  # 1/ti duration signal
+
+        ax3 = fig.add_subplot(413)  # Initialize the third plot signal in frequency domain
+        plt.ylabel("Amplitude")
+        plt.xlabel("Frequency [Hz]")
+
+        for p1, p2 in zip(f, r):  # Plot the frequency domain and polar
+            ax2.plot([0, np.angle(p2)], [0, abs(p2)], marker='o')
+            ax3.bar(p1, abs(p2), width=1.5)  # 1 / N is a normalization factor
+            plt.pause(.02)
+
+        ax4 = fig.add_subplot(414) # plot the current left/right values
+        plt.scatter(left_side, np.zeros_like(left_side), alpha=.9)
+        plt.scatter(right_side, np.zeros_like(right_side), alpha=.4)
+
+        plt.yticks([])
+
+        plt.show()
+
     return r
 
 
-master = tk.Tk() # create Tkinter object
+master = tk.Tk()  # create Tkinter object
 
 # input labels
 tk.Label(master, text="Frequency (Hz) Sinusoid 1").grid(row=0)
@@ -80,16 +112,16 @@ master.mainloop()
 
 
 fs = 500  # (SAMPLING RATE HZ)
-# f = 40  # (Frequency HZ)
-
 t = np.linspace(0, 0.5, fs)  # (SAMPLING PERIOD s)
 
 s = am1 * np.sin(fr1 * 2 * np.pi * t) + am2 * np.sin(fr2 * 2 * np.pi * t)  # (SINE WAVE)
 sn = s.size  # Signal samples
 
 root = math.e ** (2 * math.pi * 1j / sn)  # root of unity
-fft = fast_fourier_transform(s, root)  # get the signal in frequency domain
+fft = fast_fourier_transform(s, root, 2)  # get the signal in frequency domain
 
+
+'''
 fig = plt.figure(figsize=(5, 10))  # Initialize the figure
 
 ax2 = fig.add_subplot(312, projection='polar')  # Initialize the second polar plot
@@ -112,3 +144,4 @@ for f1, f2 in zip(f, fft):  # Plot the frequency domain and polar
     plt.pause(.02)
 
 plt.show()
+'''
